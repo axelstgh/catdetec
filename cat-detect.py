5b65bc5b817de24c576632ecaf15d87132988c15
axelstgh <axel.st.st@gmail.com>
2024-05-30 23:20:02 -0300

    Update cat-detect.py

--- cat-detect.py
+++ cat-detect.py
@@ -17,8 +17,24 @@ print("Iniciando captura del streaming....")
 # Video captura 
 cap = cv2.VideoCapture("rtsp://158.23.168.30:8554/streamig/hello")
 #cap = cv2.VideoCapture(0)
-print("Iniciando analisis del video.....")
-# analisis del video 
+
+if not cap.isOpened():
+    print("Error: No se puede abrir el flujo de video")
+    exit()
+
+# Obtén el ancho y alto del video
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+# Inicializa el VideoWriter
+output_path = 'C:/Users/David/Documents/David Contreras/UNLAM/2024/Project/Yolo/Object Detection 1/screenshots/output_video.avi'
+fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec para AVI
+out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+
+print("Iniciando análisis del video.....")
+# Inicializa el contador de imágenes
+image_counter = 0
 
 while(True):
     print("lectura de frames..")
@@ -29,17 +45,26 @@ while(True):
     detect = model(frame)
     print("deteción..")
 
-    # obtenemos info de las imagens detectadas
+    # Renderiza las detecciones
+    rendered_frame = np.squeeze(detect.render())
 
+    # Guarda el fotograma procesado en el archivo de video
+    out.write(rendered_frame)
+
+    # Genera un nombre de archivo único usando el contador
+    file_path = f'C:/Users/David/Documents/David Contreras/UNLAM/2024/Project/Yolo/Object Detection 1/screenshots/ss_{image_counter:04d}.jpeg'
+    cv2.imwrite(file_path, rendered_frame)
+    
     # Mostramos los FPS
-    cv2.imshow('Detector de Gatos', np.squeeze(detect.render()))
-    cv2.imwrite('C:/Users/David/Documents/David Contreras/UNLAM/2024/Project/Yolo/Object Detection 1/screenshots/ss.jpeg', np.squeeze(detect.render()) )
+    cv2.imshow('Detector de Gatos', rendered_frame)
 
-    #lee el teclad, para terminar presionamos la tecla scape
-    t = cv2.waitKey(5)
-    if t == 27: 
+    image_counter += 1
+
+    # Lee el teclado, para terminar presionamos la tecla Escape
+    if cv2.waitKey(5) == 27: 
         break
 
 # Finalizamos el programa  liberando los recursos
 cap.release()
+out.release()
 cv2.destroyAllWindows()
